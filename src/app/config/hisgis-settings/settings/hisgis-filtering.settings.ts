60bc2c6f7b2221268b6647e1789fd4cc201ef782
Simon Dirks <mail@simondirks.com>
2025-11-24 15:29:29 +0100

    Add type filter

--- src/app/config/hisgis-settings/settings/hisgis-filtering.settings.ts
+++ src/app/config/hisgis-settings/settings/hisgis-filtering.settings.ts
@@ -3,4 +3,12 @@ import { defaultSettings } from '../../default-settings/default-settings';
 
 export const hisgisFilteringSettings: FilteringSettings = {
   ...defaultSettings.filtering,
+  filterOptions: {
+    type: {
+      label: 'Type',
+      fieldIds: ['http://www.w3.org/1999/02/22-rdf-syntax-ns#type.keyword'],
+      values: [],
+      hideValueIds: [],
+    },
+  },
 };
