71cb04a846f2b5e07e6b27a0d83feac511ad6fe7
Simon Dirks <mail@simondirks.com>
2025-11-24 14:57:08 +0100

    Add basic Leaflet map

--- src/app/components/custom-render-components/by-predicate/as-wkt/asWkt.component.ts
+++ src/app/components/custom-render-components/by-predicate/as-wkt/asWkt.component.ts
@@ -1,7 +1,13 @@
 import { JsonPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../../services/api.service';
-import { SparqlService } from '../../../../services/sparql.service';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as L from 'leaflet';
 import { NodeLinkComponent } from '../../../features/node/node-link/node-link.component';
 import { PredicateRenderComponent } from '../predicate-render-component.directive';
 
@@ -12,13 +18,40 @@ import { PredicateRenderComponent } from '../predicate-render-component.directiv
   templateUrl: './asWkt.component.html',
   styleUrl: './asWkt.component.scss',
 })
-export class AsWktComponent extends PredicateRenderComponent implements OnInit {
-  constructor(
-    public api: ApiService,
-    public sparql: SparqlService,
-  ) {
+export class AsWktComponent
+  extends PredicateRenderComponent
+  implements OnInit, AfterViewInit
+{
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
+  private map: L.Map | null = null;
+
+  constructor(private ngZone: NgZone) {
     super();
   }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.initMap();
+    });
+  }
+
+  private initMap(): void {
+    if (!this.mapContainer) {
+      console.error('Map container not found');
+      return;
+    }
+
+    this.map = L.map(this.mapContainer.nativeElement).setView(
+      [51.505, -0.09],
+      13,
+    );
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+  }
 }
