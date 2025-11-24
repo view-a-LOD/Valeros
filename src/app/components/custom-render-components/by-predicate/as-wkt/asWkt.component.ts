d8f22edabd8b8db9f33fb8b8f38173611e63097e
Simon Dirks <mail@simondirks.com>
2025-11-24 14:58:46 +0100

    Update Leaflet tile layer

--- src/app/components/custom-render-components/by-predicate/as-wkt/asWkt.component.ts
+++ src/app/components/custom-render-components/by-predicate/as-wkt/asWkt.component.ts
@@ -48,10 +48,14 @@ export class AsWktComponent
       13,
     );
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
+    L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png',
+      {
+        minZoom: 0,
+        maxZoom: 20,
+        attribution:
+          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    ).addTo(this.map);
   }
 }
