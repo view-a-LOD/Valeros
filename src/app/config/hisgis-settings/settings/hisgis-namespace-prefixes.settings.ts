aad297ef075829e77ce91ed366ba1bceafddeb28
Simon Dirks <mail@simondirks.com>
2025-11-24 15:29:32 +0100

    Update hisgis-namespace-prefixes.settings.ts

--- src/app/config/hisgis-settings/settings/hisgis-namespace-prefixes.settings.ts
+++ src/app/config/hisgis-settings/settings/hisgis-namespace-prefixes.settings.ts
@@ -2,7 +2,7 @@ import { namespacePrefixes } from '../../default-settings/settings/namespace-pre
 
 export const hisgisNamespacePrefixes: Record<string, string> = {
   ...namespacePrefixes,
-  'https://hisgis.hualab.nl/id/': 'id:',
+  'https://hisgis.hualab.nl/id/': '',
   'https://hisgis.hualab.nl/def/': '',
   'http://www.opengis.net/ont/geosparql#': 'geo:',
 };
