a0e59c09f383d480cb0f946d51cbe69127fc140f
Simon Dirks <mail@simondirks.com>
2025-12-10 11:04:16 +0100

    Tweak external link styling

--- src/app/components/features/node/node-render-components/predicate-render-components/external-link/external-link.component.ts
+++ src/app/components/features/node/node-render-components/predicate-render-components/external-link/external-link.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+import { featherArrowUpRight } from '@ng-icons/feather-icons';
 import { PredicateRenderComponent } from '../../../../../../_custom-components/custom-render-components/by-predicate/predicate-render-component.directive';
 
 @Component({
   selector: 'app-external-link',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './external-link.component.html',
 })
 export class ExternalLinkComponent extends PredicateRenderComponent {
   get externalUrl(): string {
     return this.data?.value ?? '';
   }
+
+  get externalDomain(): string {
+    const raw = this.externalUrl.trim();
+    if (!raw) {
+      return '';
+    }
+
+    try {
+      const url = new URL(raw);
+      return url.hostname.replace(/^www\./, '');
+    } catch {
+      return raw;
+    }
+  }
+
+  protected readonly featherArrowUpRight = featherArrowUpRight;
 }
