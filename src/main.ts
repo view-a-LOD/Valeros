cd5fd09c3b226f6a5cb4e3f4a2f7ee3535d4e130
Simon Dirks <mail@simondirks.com>
2026-01-07 15:56:21 +0100

    Register Dutch locale

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { registerLocaleData } from '@angular/common';
+import localeNl from '@angular/common/locales/nl';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+registerLocaleData(localeNl);
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
