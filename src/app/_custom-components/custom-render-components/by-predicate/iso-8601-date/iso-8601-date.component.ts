6fd21193cb09f1f5cbf26bccd5044cbea836ec7f
Simon Dirks <mail@simondirks.com>
2026-01-07 15:58:59 +0100

    Optionally show datetime input for iso 8601

--- src/app/_custom-components/custom-render-components/by-predicate/iso-8601-date/iso-8601-date.component.ts
+++ src/app/_custom-components/custom-render-components/by-predicate/iso-8601-date/iso-8601-date.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PredicateRenderComponent } from '../predicate-render-component.directive';
 
 @Component({
@@ -8,9 +8,28 @@ import { PredicateRenderComponent } from '../predicate-render-component.directiv
   imports: [DatePipe],
   templateUrl: './iso-8601-date.component.html',
 })
-export class Iso8601DateComponent
-  extends PredicateRenderComponent
-  implements OnInit
-{
-  ngOnInit(): void {}
+export class Iso8601DateComponent extends PredicateRenderComponent {
+  private _pad2(value: number): string {
+    return value.toString().padStart(2, '0');
+  }
+
+  get dateTimeLocalValue(): string {
+    const rawValue = this.data?.value;
+    if (!rawValue) {
+      return '';
+    }
+
+    const date = new Date(rawValue);
+    if (Number.isNaN(date.getTime())) {
+      return '';
+    }
+
+    const year = date.getFullYear();
+    const month = this._pad2(date.getMonth() + 1);
+    const day = this._pad2(date.getDate());
+    const hours = this._pad2(date.getHours());
+    const minutes = this._pad2(date.getMinutes());
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  }
 }
