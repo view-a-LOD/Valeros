8f42f310451eb3cb0b5d600b62323a397002993a
Simon Dirks <mail@simondirks.com>
2026-01-07 11:14:34 +0100

    Split NADD settings for readability

--- src/app/config/nadd/nadd-settings.ts
+++ src/app/config/nadd/nadd-settings.ts
@@ -5,115 +5,119 @@ import { ViewModeSetting } from '../../models/settings/view-mode-setting.enum';
 import { ViewMode } from '../../models/view-mode.enum';
 import { defaultSettings } from '../default-settings/default-settings';
 
-export const naddSettings: SettingsModel = {
-  ...defaultSettings,
-  endpoints: {
-    ...defaultSettings.endpoints,
-    data: {
-      haagsHistorischMuseum: {
-        label: 'Haags Historisch Museum/Rijksmuseum De Gevangenpoort',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q11722011' }],
-      },
-      stichtingVanDerWyckDeKempenaer: {
-        label: 'Stichting Van der Wyck-de Kempenaer',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q20829167' }],
-      },
-      museumMohlmann: {
-        label: 'Museum Møhlmann',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2168069' }],
-      },
-      mowMuseumWesterwolde: {
-        label: 'MOW Museum Westerwolde',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q13612139' }],
-      },
-      museumAanDeA: {
-        label: 'Museum aan de A',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2004350' }],
-      },
-      keramiekMuseum: {
-        label: 'Keramiekmuseum Tiendschuur',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2022136' }],
-      },
-      discoveryMuseum: {
-        label: 'Discovery Museum',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q110996022' }],
-      },
-      stichtingDesign: {
-        label: 'Stichting Design en Kunst Openbare Ruimte (DKOR)',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q124789020' }],
-      },
-      nationaalBusMuseum: {
-        label: 'Nationaal Bus Museum',
-        endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2575273' }],
-      },
+export const naddSettings: SettingsModel = defaultSettings;
+
+naddSettings.endpoints = {
+  ...defaultSettings.endpoints,
+  data: {
+    haagsHistorischMuseum: {
+      label: 'Haags Historisch Museum/Rijksmuseum De Gevangenpoort',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q11722011' }],
+    },
+    stichtingVanDerWyckDeKempenaer: {
+      label: 'Stichting Van der Wyck-de Kempenaer',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q20829167' }],
+    },
+    museumMohlmann: {
+      label: 'Museum Møhlmann',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2168069' }],
+    },
+    mowMuseumWesterwolde: {
+      label: 'MOW Museum Westerwolde',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q13612139' }],
+    },
+    museumAanDeA: {
+      label: 'Museum aan de A',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2004350' }],
+    },
+    keramiekMuseum: {
+      label: 'Keramiekmuseum Tiendschuur',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2022136' }],
+    },
+    discoveryMuseum: {
+      label: 'Discovery Museum',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q110996022' }],
+    },
+    stichtingDesign: {
+      label: 'Stichting Design en Kunst Openbare Ruimte (DKOR)',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q124789020' }],
+    },
+    nationaalBusMuseum: {
+      label: 'Nationaal Bus Museum',
+      endpointUrls: [{ sparql: 'https://sparql.ldmax.nl/q2575273' }],
     },
   },
-  viewModes: {
+};
+
+naddSettings.viewModes = {
+  [ViewMode.List]: {
+    ...defaultSettings.viewModes[ViewMode.List],
+    [ViewModeSetting.ShowTypes]: false,
+    [ViewModeSetting.ShowOrganization]: true,
+  },
+  [ViewMode.Grid]: {
+    ...defaultSettings.viewModes[ViewMode.Grid],
+    [ViewModeSetting.ShowTypes]: false,
+    [ViewModeSetting.ShowOrganization]: true,
+  },
+};
+
+naddSettings.predicateVisibility = {
+  byViewMode: {
     [ViewMode.List]: {
-      ...defaultSettings.viewModes[ViewMode.List],
-      [ViewModeSetting.ShowTypes]: false,
-      [ViewModeSetting.ShowOrganization]: true,
+      [PredicateVisibility.SearchHits]: [
+        {
+          predicates: [
+            'https://schema.org/description',
+            'https://schema.org/material',
+            // '*',
+          ],
+        },
+      ],
+      [PredicateVisibility.Details]: [{ predicates: ['*'] }],
+      [PredicateVisibility.Hide]: [{ predicates: [] }],
     },
     [ViewMode.Grid]: {
-      ...defaultSettings.viewModes[ViewMode.Grid],
-      [ViewModeSetting.ShowTypes]: false,
-      [ViewModeSetting.ShowOrganization]: true,
+      [PredicateVisibility.SearchHits]: [],
+      [PredicateVisibility.Details]: [{ predicates: ['*'] }],
+      [PredicateVisibility.Hide]: [],
     },
   },
-  predicateVisibility: {
-    byViewMode: {
-      [ViewMode.List]: {
-        [PredicateVisibility.SearchHits]: [
-          {
-            predicates: [
-              'https://schema.org/description',
-              'https://schema.org/material',
-              // '*',
-            ],
-          },
-        ],
-        [PredicateVisibility.Details]: [{ predicates: ['*'] }],
-        [PredicateVisibility.Hide]: [{ predicates: [] }],
-      },
-      [ViewMode.Grid]: {
-        [PredicateVisibility.SearchHits]: [],
-        [PredicateVisibility.Details]: [{ predicates: ['*'] }],
-        [PredicateVisibility.Hide]: [],
-      },
-    },
-    alwaysHide: [...defaultSettings.predicateVisibility.alwaysHide],
-    hideTypeBadges: [],
-  },
-  filtering: {
-    ...defaultSettings.filtering,
-    showOrganizationsFilter: true,
-    filterOptions: {
-      type: {
-        label: 'Type',
-        fieldIds: ['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'],
-        values: [],
-      },
+  alwaysHide: [...defaultSettings.predicateVisibility.alwaysHide],
+  hideTypeBadges: [],
+};
+
+naddSettings.filtering = {
+  ...defaultSettings.filtering,
+  showOrganizationsFilter: true,
+  filterOptions: {
+    type: {
+      label: 'Type',
+      fieldIds: ['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'],
+      values: [],
     },
   },
-  predicates: {
-    ...defaultSettings.predicates,
-    files: [
-      'https://schema.org/associatedMedia',
-      'https://schema.org/contentUrl',
-    ],
-    hopFiles: [
-      ['https://schema.org/associatedMedia', 'https://schema.org/contentUrl'],
-    ],
-  },
-  nodeVisibility: {
-    ...defaultSettings.nodeVisibility,
-    // TODO: Implement SPARQL search provider support for this
-    alwaysHide: {
-      hideTerms: {
-        fieldIds: ['https://schema.org/inDefinedTermSet'],
-        valueIds: [],
-        type: FilterType.FieldAndValue,
-      },
+};
+
+naddSettings.predicates = {
+  ...defaultSettings.predicates,
+  files: [
+    'https://schema.org/associatedMedia',
+    'https://schema.org/contentUrl',
+  ],
+  hopFiles: [
+    ['https://schema.org/associatedMedia', 'https://schema.org/contentUrl'],
+  ],
+};
+
+naddSettings.nodeVisibility = {
+  ...defaultSettings.nodeVisibility,
+  // TODO: Implement SPARQL search provider support for this
+  alwaysHide: {
+    hideTerms: {
+      fieldIds: ['https://schema.org/inDefinedTermSet'],
+      valueIds: [],
+      type: FilterType.FieldAndValue,
     },
   },
 };
