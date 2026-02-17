9864cf2c10955f1362ad5add4732e3e507df4f0a
Simon Dirks <mail@simondirks.com>
2026-02-17 14:10:21 +0100

    Add initial search query model (closes #214)

---
+++ libs/shared/types/src/lib/search-query.types.ts
@@ -0,0 +1,57 @@
+/** @format */
+
+export type EndpointType = 'sparql' | 'qlever' | 'elastic';
+
+export type EndpointConfig = {
+  type: EndpointType;
+  url: string;
+};
+
+export type SearchQueryModel = {
+  query: string;
+  endpoints: EndpointConfig[];
+  filters: SearchQueryFilterModel[];
+  sorting: SortingModel;
+  prefixes?: PrefixModel[];
+  retrieve?: RetrieveConfig;
+};
+
+export type SearchQueryFilterModel = {
+  predicates?: string[];
+  objects?: string[];
+};
+
+export type SortingModel = {
+  predicates: string[];
+  direction: 'asc' | 'desc';
+};
+
+export type PrefixModel = {
+  prefix: string;
+  namespace: string;
+};
+
+export type NodeScope = 'roots' | 'all';
+
+export type PropertyPath = string; // SPARQL property path syntax with prefixes or <full_urls>
+
+export type Prefix = {
+  prefix: string;
+  namespace: string;
+};
+
+export type PathSelector = {
+  path: PropertyPath;
+  languages?: string[];
+  includeHighlights?: boolean;
+};
+
+export type RetrieveSelector = {
+  scope: NodeScope;
+  paths: PathSelector[];
+};
+
+export type RetrieveConfig = {
+  selectors: RetrieveSelector[];
+  prefixes?: Prefix[];
+};
