e025549d859e7c1b013b499bf509ef1045e9c703
Simon Dirks <mail@simondirks.com>
2026-02-17 16:43:58 +0100

    Remove empty constructor

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor() {}
-
   @Get('health')
   getHealth(): { status: string } {
     return { status: 'ok' };
