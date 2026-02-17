4886587dd5231fae7a5b14bff5b309b4a9160680
Simon Dirks <mail@simondirks.com>
2026-02-17 10:17:12 +0100

    Wait for shared-types build for back-end, add ESLint

--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/types';
+// TODO: Remove these types from front-end
+export * from './lib/filter.types';
+export * from './lib/node.types';
+export * from './lib/search.types';
--- libs/shared/types/src/lib/types.ts
+++
@@ -1,5 +0,0 @@
-// Shared types between frontend and backend
-// TODO: Remove these types from front-end
-export * from './filter.types';
-export * from './node.types';
-export * from './search.types';
